2115dc560c78040f9dfab41a2cf917552974c1df
Marvin Ren <marvin.ren@yale.edu>
2021-11-01 21:32:19 -0400

    erro

---
+++ db/schema.sql
@@ -0,0 +1,26 @@
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS managers;
+
+CREATE TABLE managers (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+)
+
+CREATE TABLE departments (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+)
+
+
+CREATE TABLE employees (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    department VARCHAR(30) NOT NULL,
+    salary INTEGER,
+    manager VARCHAR(30), 
+    CONSTRAINT fk_department FOREIGN KEY (department) REFERENCES departments(id) ON DELETE SET NULL,
+    CONSTRAINT fk_manager FOREIGN KEY (manager) REFERENCES managers(id) ON DELETE SET NULL
+)
+

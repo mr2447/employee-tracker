fe5979e64b2338a68dc33a94e59551bc0fc3a825
Marvin Ren <marvin.ren@yale.edu>
2021-11-02 17:09:49 -0400

    alias

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,8 +6,9 @@ VALUES
 INSERT INTO roles (title, salary, department_id)
 VALUES
 ('sales lead', '100000', '1'),
-('junior', '200000', '2');
+('junior tech', '200000', '2');
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES 
-('James', 'Fraser', '1', '1');
+('James', 'Fraser', '1', '1'),
+('Marvin', 'Ren', '2', '1');

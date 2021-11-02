7eaf6ec6ddbf2cb3f6879409aab399d9349894ac
Marvin Ren <marvin.ren@yale.edu>
2021-11-02 01:48:13 -0400

    error

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,14 +1,13 @@
-INSERT INTO employees (first_name, last_name, role_id, manager_id)
+INSERT INTO departments (name)
 VALUES 
-('James', 'Fraser', '1', '1');
+('tech'),
+('music');
 
 INSERT INTO roles (title, salary, department_id)
 VALUES
 ('sales', '100000', '1'),
 ('web-dev', '200000', '1');
 
-INSERT INTO departments (name)
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES 
-('tech'),
-('music');
-
+('James', 'Fraser', '1', '1');
